cbf7793bbf68c2ed141e490e1c6f956d1d32a2f3
Sunvin19 <97520458+Sunvin19@users.noreply.github.com>
2022-01-28 18:47:48 +0100

    Revert "Update Viaje.java"

--- planetas/src/main/java/com/mycompany/planetas/Viaje.java
+++ planetas/src/main/java/com/mycompany/planetas/Viaje.java
@@ -12,21 +12,18 @@ import java.util.Scanner;
  * @author Hogar
  */
 public class Viaje {
-    
-    // Declarración de variables a utilizar
+
     String planeta;
     double m, f;
     char N;
 
     public void calculoPlaneta() {
-        
-        // Apertura del ciclo
+
         do {
-            System.out.println("Por favor ingresa el nombre de un planeta para comenzar el cálculo\n1.Marte\n2.Tierra\n3.Jupiter\n4.Saturno\n5.Pluton\n6.Neptuno\nEscriba aquí:");
+            System.out.println("Ingresa el nombre de un planeta para comenzar el calculo\n1.Marte\n2.Tierra\n3.Jupiter\n4.Saturno\n5.Pluton\nEscriba aqui:");
             Scanner st = new Scanner(System.in);
             planeta = st.nextLine();
-            
-            // Apertura del ciclo switch para elegir 
+
             switch (planeta) {
 
                 case "Marte","marte","1":
@@ -34,9 +31,9 @@ public class Viaje {
                     System.out.println("\n Ingrese la masa en 'Kg' para calcular su peso en marte");
                     Scanner sr = new Scanner(System.in);
                     this.m = sr.nextDouble();
-                    // operación cálculo de Marte usando la segunda ley de Newton
+                    // operacion calculo Marte usando la segunda ley de Newton
                     this.f = m * (3.72);
-                    // se envía a imprimir
+                    //se envia a imprimir
                     System.out.println("El peso del astronauta en marte es de: " + this.f + "N");
                     break;
                 case "Tierra","tierra","2":
@@ -44,9 +41,9 @@ public class Viaje {
                     System.out.println("\n Ingrese la masa en 'Kg' para calcular su peso en la tierra");
                     Scanner sq = new Scanner(System.in);
                     this.m = sq.nextDouble();
-                    // operación cálculo de Tierra usando la segunda ley de Newton
+                    // operacion calculo Marte usando la segunda ley de Newton
                     this.f = m * (9.81);
-                    // se envía a imprimir
+                    //se envia a imprimir
                     System.out.println("El peso del astronauta en tierra es de: " + this.f + "N");
                     break;
                 case "Jupiter","jupiter","3":
@@ -54,9 +51,9 @@ public class Viaje {
                     System.out.println("\n Ingrese la masa en 'Kg' para calcular su peso en jupiter");
                     Scanner sw = new Scanner(System.in);
                     this.m = sw.nextDouble();
-                    // operación de cálculo Jupiter usando la segunda ley de Newton
+                    // operacion calculo Marte usando la segunda ley de Newton
                     this.f = m * (24.79);
-                    // se envía a imprimir
+                    //se envia a imprimir
                     System.out.println("El peso del astronauta en jupiter es de: " + this.f + "N");
                     break;
                 case "Saturno","saturno","4":
@@ -64,9 +61,9 @@ public class Viaje {
                     System.out.println("\n Ingrese la masa en 'Kg' para calcular su peso en saturno");
                     Scanner se = new Scanner(System.in);
                     this.m = se.nextDouble();
-                    // operación de cálculo Saturno usando la segunda ley de Newton
+                    // operacion calculo Marte usando la segunda ley de Newton
                     this.f = m * (10.44);
-                    // se envia a imprimir
+                    //se envia a imprimir
                     System.out.println("El peso del astronauta en saturno es de: " + this.f + "N");
                     break;
                 case "Pluton","pluton","5":
@@ -74,29 +71,18 @@ public class Viaje {
                     System.out.println("\n Ingrese la masa en 'Kg' para calcular su peso en pluton");
                     Scanner sy = new Scanner(System.in);
                     this.m = sy.nextDouble();
-                    // operación cálculo de Pluton usando la segunda ley de Newton
+                    // operacion calculo Marte usando la segunda ley de Newton
                     this.f = m * (0.62);
-                    // se envía a imprimir
+                    //se envia a imprimir
                     System.out.println("El peso del astronauta en pluton es de: " + this.f + "N");
                     break;
-                    case "Neptuno","neptuno","6":
-
-                    System.out.println("\n Ingrese la masa en 'Kg' para calcular su peso en neptuno");
-                    Scanner sy = new Scanner(System.in);
-                    this.m = sy.nextDouble();
-                    // operación cálculo de Neptuno usando la segunda ley de Newton
-                    this.f = m * (11.15);
-                    // se envía a imprimir
-                    System.out.println("El peso del astronauta en neptuno es de: " + this.f + "N");
-                    break;
-
 
             }
             // solicitud para continuar o cerrar
-            System.out.println("\n¿Quiere continuar?");
+            System.out.println("\n quieres continuar");
             this.N = st.next().charAt(0);// mensaje de salida
-        } while (N != 'n' && N != 'N');// cierre del ciclo
-        System.out.println("\nUsted ha salido del Sistema");
+        } while (N != 'n' && N != 'N');
+        System.out.println("\nusted ha salido del Sistema");
 
     }
 
